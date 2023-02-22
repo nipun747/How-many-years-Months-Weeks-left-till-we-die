2fb9dd30e2250fca60d16fcb091a5240c244bb3c
Samina Afroze <60169903+nipun747@users.noreply.github.com>
2023-02-22 04:54:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+current_age=int(input("what's your age"))
+years_left=90-current_age
+months_left=years_left*12
+weeks_left=years_left*52
+days_left=years_left*365
+print(f"years left {years_left} months left {months_left} weeks left  {weeks_left} days left  {days_left}")
